89bee68f5386d5119f6eb62f328ae72df7eeb651
ryan m <contact@ryanmeek.com>
2020-01-20 21:59:35 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Mr. Chocolate', 20, 'M', 'brown', 'kind', 0),
+('Rowdy', 10, 'M', 'grey', 'motivated', 1),
+('Tabitha', 6, 'F', 'dark brown', 'loner', 1),
+('Sargeant Brown', 7, 'dark brown', 'M', 'wild card', 0),
+('Melissa', 13, 'F', 'dark brown', 'agressive', 1),
+('Grinch', 2, 'M', 'dark brown', 'goofy', 1),
+('Wendy', 6, 'F', 'white', 'playful', 1),
+(NULL, 20, 'M', 'black', 'aggressive', 0);
--- lib/seed.sql
+++ lib/seed.sql
@@ -1,9 +1,9 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
-(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
-(2,"Rowdy", 10, "M", "black", "intense", 1),
-(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
-(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
-(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
-(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
-(7,"Wendy", 6, "F", "Blue", "naive", 1),
-(8,null, 20, "M", "black", "aggressive", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Mr. Chocolate', 20, 'M', 'brown', 'kind', 0),
+('Rowdy', 10, 'M', 'grey', 'motivated', 1),
+('Tabitha', 6, 'F', 'dark brown', 'loner', 1),
+('Sargeant Brown', 7, 'dark brown', 'M', 'wild card', 0),
+('Melissa', 13, 'F', 'dark brown', 'agressive', 1),
+('Grinch', 2, 'M', 'dark brown', 'goofy', 1),
+('Wendy', 6, 'F', 'white', 'playful', 1),
+(NULL, 20, 'M', 'black', 'aggressive', 0);
