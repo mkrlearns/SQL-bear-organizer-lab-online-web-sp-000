fe0d7325c937906371c9d97bc0108af65ede79da
ryan m <contact@ryanmeek.com>
2020-01-20 21:53:00 +0000

    Automatically backed up by Learn

---
+++ lib/seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Mr. Chocolate', 1, 'M', 'brown', 'kind', 1),
+('Rowdy', 3, 'F', 'black', 'motivated', 0),
+('Tabitha', 6, 'F', 'green', 'loner', 1),
+('Sargeant Brown', 7, 'red', 'M', 'wild card', 0),
+('Melissa', 13, 'F', 'brown', 'agressive', 1),
+('Grinch', 2, 'M', 'black', 'goofy', 1),
+('Wendy', 6, 'F', 'white', 'playful', 1),
+(NULL, 2, 'F', 'grey', 'rabid', 0);
